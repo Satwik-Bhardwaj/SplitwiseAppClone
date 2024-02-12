1965ea7108b6a285c825f31d900cf1014164cf26
Satwik Bhardwaj <satwikbhardwaj123@gmail.com>
2024-02-12 11:43:20 +0530

    New Project initiated
    Added new project to git/github

---
+++ src/main/java/com/satwik/splitwiseclone/SplitwiseAppCloneApplication.java
@@ -0,0 +1,13 @@
+package com.satwik.splitwiseclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SplitwiseAppCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SplitwiseAppCloneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/satwik/splitwiseclone/SplitwiseAppCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.satwik.splitwiseclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SplitwiseAppCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
