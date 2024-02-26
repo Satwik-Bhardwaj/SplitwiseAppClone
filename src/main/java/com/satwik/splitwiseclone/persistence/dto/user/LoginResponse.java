1c7fc222f88c29b68881b0d736d7472e0f6e4635
Satwik-Bhardwaj <satwikbhardwaj123@gmail.com>
2024-02-26 12:43:02 +0530

    few changes

--- src/main/java/com/satwik/splitwiseclone/persistence/dto/user/LoginResponse.java
+++ src/main/java/com/satwik/splitwiseclone/persistence/dto/user/LoginResponse.java
@@ -1,5 +1,6 @@
 package com.satwik.splitwiseclone.persistence.dto.user;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,7 +10,10 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class LoginResponse {
 
+
+    @NotNull
     String token;
 
+    @NotNull
     String message;
 }
