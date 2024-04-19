f5acb54a64f97bd7027669991e9331e1c866fc8d
Satwik Bhardwaj <satwikbhardwaj123@gmail.com>
2024-04-19 23:30:00 +0530

    working on decoding idtoken (google client oauth2)

--- src/main/java/com/satwik/splitwiseclone/controller/OAuthController.java
+++ src/main/java/com/satwik/splitwiseclone/controller/OAuthController.java
@@ -3,6 +3,12 @@ package com.satwik.splitwiseclone.controller;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken;
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdTokenVerifier;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.gson.GsonFactory;
 import com.satwik.splitwiseclone.configuration.jwt.JwtUtil;
 import io.jsonwebtoken.Claims;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +19,9 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -73,7 +82,7 @@ public class OAuthController {
         RestTemplate restTemplate = new RestTemplate();
         ResponseEntity<String> response = restTemplate.exchange(TOKEN_URI, HttpMethod.POST, requestEntity, String.class);
 
-        // decoding token
+        // getting token
         String json_response = response.getBody();
         ObjectMapper objectMapper1 = new ObjectMapper();
         JsonNode jsonNode;
@@ -82,8 +91,47 @@ public class OAuthController {
         } catch (JsonProcessingException e) {
             throw new RuntimeException(e);
         }
-        String idToken = jsonNode.get("id_token").asText();
-        Claims userClaims = jwtUtil.getClaimsOfOAuth2Token(idToken, CLIENT_SECRET);
+        String id_token = jsonNode.get("id_token").asText();
+
+
+        HttpTransport transport = new NetHttpTransport();
+        JsonFactory jsonFactory = new GsonFactory();
+        GoogleIdTokenVerifier verifier = new GoogleIdTokenVerifier.Builder(transport, jsonFactory)
+                // Specify the CLIENT_ID of the app that accesses the backend:
+                .setAudience(Collections.singletonList(CLIENT_ID))
+                // Or, if multiple clients access the backend:
+                //.setAudience(Arrays.asList(CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3))
+                .build();
+
+        GoogleIdToken idToken = null;
+        try {
+            idToken = verifier.verify(id_token);
+        } catch (GeneralSecurityException | IOException e) {
+            throw new RuntimeException(e);
+        }
+        if (idToken != null) {
+            GoogleIdToken.Payload payload = idToken.getPayload();
+
+            // Print user identifier
+            String userId = payload.getSubject();
+            System.out.println("User ID: " + userId);
+
+            // Get profile information from payload
+            String email = payload.getEmail();
+            boolean emailVerified = Boolean.valueOf(payload.getEmailVerified());
+            String name = (String) payload.get("name");
+            String pictureUrl = (String) payload.get("picture");
+            String locale = (String) payload.get("locale");
+            String familyName = (String) payload.get("family_name");
+            String givenName = (String) payload.get("given_name");
+
+            // Use or store profile information
+            // ...
+
+        } else {
+            System.out.println("Invalid ID token.");
+        }
+        Claims userClaims = jwtUtil.getClaimsOfOAuth2Token(id_token, CLIENT_SECRET);
         String user = userClaims.getSubject();
         return ResponseEntity.ok(user);
     }
