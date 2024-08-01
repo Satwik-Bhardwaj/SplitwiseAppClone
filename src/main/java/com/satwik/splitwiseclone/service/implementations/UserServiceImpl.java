0803f14ae8fb7d679bf2e43134252abd6c332a2c
arihantongraph <arihant@apporto.com>
2024-08-01 12:45:42 +0530

    an unused commit 2

--- src/main/java/com/satwik/splitwiseclone/service/implementations/UserServiceImpl.java
+++ src/main/java/com/satwik/splitwiseclone/service/implementations/UserServiceImpl.java
@@ -18,7 +18,7 @@ import org.springframework.stereotype.Service;
 import java.util.UUID;
 
 @Service
-public class UserServiceImpl implements UserService  {
+public class UserServiceImpl implements UserService {
 
     @Autowired
     private AuthorizationService authorizationService;
